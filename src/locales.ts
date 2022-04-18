4d81d65f87ea1aedbef2089197393ec4c5eb5bd1
Qreepex <65496825+Qreepex@users.noreply.github.com>
2022-04-18 22:11:31 +0200

    added locales, base config

--- src/locales.ts
+++ src/locales.ts
@@ -1,31 +1,31 @@
 export default [
-  "en", // English
-  "bg", // Bulgarian
-  "zh-CN", // Chinese (China)
-  "zh-TW", // Chinese (Taiwan)
-  "hr", // Croatian
-  "cs", // Czech
-  "da", // Danish
-  "nl", // Dutch
-  "fi", // Finnish
-  "fr", // French
-  "de", // German
-  "el", // Greek
-  "hi", // Hindi
-  "hu", // Hungarian
-  "it", // Italian
-  "ja", // Japanese
-  "ko", // Korean
-  "lt", // Lithuanian
-  "no", // Norwegian
-  "pl", // Polish
-  "pt", // Portuguese
-  "ro", // Romanian
-  "ru", // Russian
-  "es", // Spanish
-  "sv-SE", // Swedish
-  "th", // Thai
-  "tr", // Turkish
-  "uk", // Ukrainian
-  "vi", // Vietnamese
+  "en",     // English
+  "bg",     // Bulgarian
+  "zh-CN",  // Chinese (China)
+  "zh-TW",  // Chinese (Taiwan)
+  "hr",     // Croatian
+  "cs",     // Czech
+  "da",     // Danish
+  "nl",     // Dutch
+  "fi",     // Finnish
+  "fr",     // French
+  "de",     // German
+  "el",     // Greek
+  "hi",     // Hindi
+  "hu",     // Hungarian
+  "it",     // Italian
+  "ja",     // Japanese
+  "ko",     // Korean
+  "lt",     // Lithuanian
+  "no",     // Norwegian
+  "pl",     // Polish
+  "pt",     // Portuguese
+  "ro",     // Romanian
+  "ru",     // Russian
+  "es",     // Spanish
+  "sv-SE",  // Swedish
+  "th",     // Thai
+  "tr",     // Turkish
+  "uk",     // Ukrainian
+  "vi",     // Vietnamese
 ];
